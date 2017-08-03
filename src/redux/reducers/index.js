da5d7c381d733d2a65a1d9072f4470f103bd016e
github username <jilustrisimo@users.noreply.github.com>
2017-08-03 01:27:29 -0400

    refactored combineReducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux'
 import searchFormReducer from './searchFormReducer'
 
-const allReducers = combineReducers({
+export default combineReducers({
   query: searchFormReducer
 })
