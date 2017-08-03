65ffcce4fb636245ebd7e8abbf1f90ece7484958
github username <jilustrisimo@users.noreply.github.com>
2017-08-03 11:52:55 -0400

    added redux-promise

--- src/redux/index.js
+++ src/redux/index.js
@@ -4,13 +4,13 @@ import {
 } from 'redux'
 import thunk from 'redux-thunk'
 import { createLogger } from 'redux-logger'
-
+import promiseMiddleware from 'redux-promise-middleware'
 import allReducers from './reducers/index'
 
 const store = createStore(
   allReducers,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(thunk, createLogger())
+  applyMiddleware(promiseMiddleware(), thunk, createLogger())
 )
 
 export default store
