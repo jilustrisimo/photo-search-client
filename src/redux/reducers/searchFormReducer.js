8a7491cd087a6014972c353f66ced58b8ce47fd4
github username <jilustrisimo@users.noreply.github.com>
2017-08-03 19:49:45 -0400

    set case for errors

--- src/redux/reducers/searchFormReducer.js
+++ src/redux/reducers/searchFormReducer.js
@@ -1,19 +1,22 @@
 const initialState = {
   fetching: false,
   fetched: false,
-  imgs: []
+  imgs: [],
+  error: null
 }
 export default (state=initialState, action) => {
   switch (action.type) {
-    case 'FETCH_IMAGES_START':
+    case 'FETCH_IMAGES_PENDING':
       return {...state, fetching: true}
-    case 'FETCH_IMAGES_END':
+    case 'FETCH_IMAGES_FULFILLED':
       return {
         ...state, 
         fetching: false,
         fetched: true,
         imgs: action.payload
       }
+    case 'FETCH_IMAGES_REJECTED':
+      return {...state, fetching: false, error: action.payload}
     default:
       return state
   }
