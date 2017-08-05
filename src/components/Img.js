44236eb9e39c482defbdab9a757c0920c3c785fe
github username <jilustrisimo@users.noreply.github.com>
2017-08-04 22:09:45 -0400

    built img.js

---
+++ src/components/Img.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Img = props =>
+  <li>
+    <a href={props.link}>
+      <img src={props.url} alt='Unsplash Image Here' />
+    </a>
+    <p>
+      Photo by
+      <a href={props.user}>{props.name}</a>
+      <a href={props.link}>See on Unsplash</a>
+    </p>
+  </li>
+
+export default Img
