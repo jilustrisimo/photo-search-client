6c8177d6e150b83c78155ded8a50bbb11f00ed74
github username <jilustrisimo@users.noreply.github.com>
2017-08-04 22:10:06 -0400

    built imglist container

---
+++ src/containers/ImgList.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import Img from '../components/Img'
+import NoImgs from '../components/NoImgs'
+import logo from './logo.svg'
+
+class ImgList extends Component {
+
+  render(){
+    this.imgs = this.props.imgs
+    let result
+    if (this.imgs.length > 0){
+      result = this.imgs.map( img =>
+        <Img
+          url={img.urls.thumb}
+          user={img.user.links.html}
+          name={img.user.name}
+          link={img.links.html}
+          key={img.id}
+        />
+      )
+    } else if (this.props.loading == true) {
+        <img src={logo} className="App-logo" alt="logo" />
+    } else {
+      result = <NoImgs/>
+    }
+
+    return (
+      <ul className="img-list">
+        {result}
+      </ul>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    loading: state.query.fetching,
+    imgs: state.query.imgs
+  }
+}
+
+export default connect(mapStateToProps)(ImgList)
