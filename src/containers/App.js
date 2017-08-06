a8e577f8641e971e7d70fa442941d6f0d9166e6b
github username <jilustrisimo@users.noreply.github.com>
2017-08-06 14:27:50 -0400

    rendering Navlink on App

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react'
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Link,
-//   Switch
-// } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch
+} from 'react-router-dom'
 
 import SearchForm from '../components/searchForm'
 import ImgList from '../containers/ImgList'
-// import logo from './logo.svg';
-import './App.css';
-// import Home from '../views/Home'
-// import Signup from '../views/Signup'
+import Navbar from '../components/Navbar'
+import Home from '../components/Home'
+import About from '../components/About'
+import './App.css'
 
 class App extends Component {
   render() {
@@ -20,7 +20,13 @@ class App extends Component {
 				<div className="main-header">
 					<div className="inner">
 						<h1 className="main-title">PhotoSearch</h1>
-             <SearchForm /> 
+             <Router>
+							 <div>
+								 <Navbar/>
+								 <Route exact path='/' render={Home}/>
+								 <Route exact path='/about' render={About}/>
+							 </div>
+						 </Router>
 					</div>
 				</div>
 				<div className="main-content">
