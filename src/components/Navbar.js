c83838156c4ebb8c08e326412ae7a9d32b33d896
github username <jilustrisimo@users.noreply.github.com>
2017-08-06 18:33:16 -0400

    fixed styling for navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,43 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { 
-  BrowserRouter as Router,
-  Route,
-  NavLink
-} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const link = {
   width: '100px',
   padding: '12px',
   margin: '0 6px 6px',
-  background: 'blue',
+  background: '#B6B6B4',
   textDecoration: 'none',
   color: 'white',
+  borderBottomLeftRadius: '10px',
+  borderBottomRightRadius: '10px',
 }
 
 const Navbar = () => 
-  <div>
+  <div className='navbar'>
     <NavLink
     to='/'
     exact
     style={link}
     activeStyle={{
-      background: 'darkblue'
+      background: '#D1D0CE'
     }}>Home</NavLink>
-    <NavLink
-    to='/about'
-    exact
-    style={link}
-    activeStyle={{
-    background: 'darkblue'
-    }}>About</NavLink>
     <NavLink
     to='/search'
     exact
     style={link}
     activeStyle={{
-      background: 'darkblue'
+      background: '#D1D0CE'
     }}>Search</NavLink>
+    <NavLink
+    to='/about'
+    exact
+    style={link}
+    activeStyle={{
+    background: '#D1D0CE'
+    }}>About</NavLink>
   </div>
 
 export default Navbar
