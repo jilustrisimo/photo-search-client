22e1b2ee1ea6ea4fa0dd6e8b430066d9c3eeb834
github username <jilustrisimo@users.noreply.github.com>
2017-08-07 10:43:08 -0400

    fixed formatting of App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import {
   BrowserRouter as Router,
   Route,
-  Switch
+	Switch
 } from 'react-router-dom'
 
 import SearchForm from '../components/searchForm'
@@ -17,20 +17,20 @@ class App extends Component {
     return (
 			<Router>
 				<Switch>
-				<div>
-					<div className="main-header">
-						<div className="inner">
-							<Navbar/>
-							<h1 className="main-title">PhotoSearch</h1>
-							<Route exact path='/search' component={SearchForm}/>
+					<div>
+						<div className="main-header">
+							<div className="inner">
+								<Navbar/>
+								<h1 className="main-title">PhotoSearch</h1>
+								<Route exact path='/search' component={SearchForm}/>
+							</div>
+						</div>
+						<div className="main-content">
+							<Route exact path='/' render={Home}/>
+							<Route exact path='/about' render={About}/>
+							<Route exact path='/search' component={ImgList}/> 
 						</div>
 					</div>
-					<div className="main-content">
-						<Route exact path='/' render={Home}/>
-						<Route exact path='/about' render={About}/>
-						 <Route exact path='/search' component={ImgList}/> 
-					</div>
-				</div>
 				</Switch>
 			</Router>
     );
