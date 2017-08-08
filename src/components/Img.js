81bcb375ccae236b957c05bdf10e05070bf3d2e0
github username <jilustrisimo@users.noreply.github.com>
2017-08-08 17:29:25 -0400

    cleaning out Img

--- src/components/Img.js
+++ src/components/Img.js
@@ -14,38 +14,18 @@ class Img extends Component {
     }
   }
 
-  // componentDidMount() {
-  //   this.handleClick()
-  // }
-
-  handleClick = (e,params) => {
-    e.preventDefault()
+  handleClick = params => {
     this.props.saveImg(params)
   }
 
   render(){
-    // const href = window.location.href
-
-    // let button
-    // if (href.indexOf('search') >= 0){
-    //   button = 
-    //     <button title='Like Photo' onClick={(e,params) => this.handleClick(e,params)}>
-    //       <i className="material-icons icn-thumb">thumb_up</i>
-    //     </button>
-    // } else {
-    //   button = null
-    // }
 
     return(
       <li>
-        <a href={this.props.link} onClick={(e) => this.handleClick(e,this.params)}>
+        <a href={this.props.link} onClick={() => this.handleClick(this.params)} target='_blank'>
           <img src={this.props.url} alt='Preview Not Available' title='View Original'/>
         </a>
         <a href={this.props.user} title='View Photographer Profile'>{this.props.name}</a>
-        {/* <button title='Like Photo' onClick={(e,params) => this.handleClick(e,params)}>
-          <i className="material-icons icn-thumb">thumb_up</i>
-        </button> */}
-        {/* {button} */}
       </li>
     )
   }  
