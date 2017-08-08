f0aa1269beca2e2cb9a5a7df9ac0a146d761c820
github username <jilustrisimo@users.noreply.github.com>
2017-08-08 14:48:38 -0400

    turning Img into container

--- src/components/Img.js
+++ src/components/Img.js
@@ -1,45 +1,58 @@
 import React, { Component } from 'react'
-import { upvote } from '../redux/actions/PhotoActions'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+import { saveImg } from '../redux/actions/ImgActions'
 
 class Img extends Component {
 
   params = {
-    // favorite: {
-    //   remote_id: this.props.key,
-    //   image_url: this.props.url,
-    // }
+    favorite: {
+      photographer: this.props.name,
+      image_url: this.props.url,
+      profile_url: this.props.user
+    }
   }
 
-  handleClick = (e, params) => {
+  // componentDidMount() {
+  //   this.handleClick()
+  // }
+
+  handleClick = (e,params) => {
     e.preventDefault()
-    this.props.upvote(params)
+    this.props.saveImg(params)
   }
 
   render(){
-    const href = window.location.href
+    // const href = window.location.href
 
-    let button
-    if (href.indexOf('search') <= 0){
-      button = 
-        <button title='Like Photo' onClick={(e,params) => this.handleClick(e,params)}>
-          <i className="material-icons icn-thumb">thumb_up</i>
-        </button>
-    } else {
-      button = null
-    }
+    // let button
+    // if (href.indexOf('search') >= 0){
+    //   button = 
+    //     <button title='Like Photo' onClick={(e,params) => this.handleClick(e,params)}>
+    //       <i className="material-icons icn-thumb">thumb_up</i>
+    //     </button>
+    // } else {
+    //   button = null
+    // }
 
     return(
       <li>
-        <a href={this.props.link}>
-          <img src={this.props.url} alt='Preview Not Available' title='Open Original'/>
+        <a href={this.props.link} onClick={(e) => this.handleClick(e,this.params)}>
+          <img src={this.props.url} alt='Preview Not Available' title='View Original'/>
         </a>
         <a href={this.props.user} title='View Photographer Profile'>{this.props.name}</a>
-        <button title='Like Photo' onClick={(e,params) => this.handleClick(e,params)}>
+        {/* <button title='Like Photo' onClick={(e,params) => this.handleClick(e,params)}>
           <i className="material-icons icn-thumb">thumb_up</i>
-        </button>
+        </button> */}
+        {/* {button} */}
       </li>
     )
   }  
 }
 
-export default Img
+const matchDispatchToProps = dispatch => {
+	return bindActionCreators({saveImg: saveImg}, dispatch)
+}
+
+export default connect(null, matchDispatchToProps)(Img)
