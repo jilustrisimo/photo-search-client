45f561a294447a9102fab977ddfc8a42bb4ea4d4
github username <jilustrisimo@users.noreply.github.com>
2017-08-09 00:04:03 -0400

    added walkthrough to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,6 +12,7 @@ const Home = () =>
     <p>
       There is also a list of recently viewed photos under the "Recent Views" tab.
     </p>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/1oVobXNEmW4" frameborder="0" allowfullscreen></iframe>
   </div>
 
 export default Home
