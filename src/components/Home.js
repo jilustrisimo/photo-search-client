2b9cb38dfebcb8fe90a7da44bc315cf734638afa
github username <jilustrisimo@users.noreply.github.com>
2017-08-08 23:47:26 -0400

    built Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,17 @@
 import React from 'react'
 
-const Home = () => <div>Home</div>
+const Home = () => 
+  <div className='center'>
+    <h3>Welcome to PhotoSearch!</h3>
+    <hr/>
+    <h4>Usage Guide</h4>
+    <p>
+      Navigate to the search tab and search for high quality photos.  
+      Clicking a photo will navigate to the website the photo is hosted on.
+    </p>
+    <p>
+      There is also a list of recently viewed photos under the "Recent Views" tab.
+    </p>
+  </div>
 
 export default Home
