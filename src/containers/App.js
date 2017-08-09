65a70a8ce2ca6afa687471542ff84c21203e6c8f
github username <jilustrisimo@users.noreply.github.com>
2017-08-08 22:23:41 -0400

    added routes to App

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,9 +5,11 @@ import {
 	Switch
 } from 'react-router-dom'
 
-import SearchForm from '../components/searchForm'
-import ImgList from '../containers/ImgList'
+import SearchForm from './searchForm'
+import ImgList from './ImgList'
 import Navbar from '../components/Navbar'
+import RecentList from './RecentList'
+import RecentHeader from '../components/RecentHeader'
 import Home from '../components/Home'
 import About from '../components/About'
 import '../styles/App.css'
@@ -23,12 +25,14 @@ class App extends Component {
 								<Navbar/>
 								<h1 className="main-title">PhotoSearch</h1>
 								<Route exact path='/search' component={SearchForm}/>
+								<Route exact path='/recent' component={RecentHeader}/>
 							</div>
 						</div>
 						<div className="main-content">
 							<Route exact path='/' render={Home}/>
 							<Route exact path='/about' render={About}/>
 							<Route exact path='/search' component={ImgList}/> 
+							<Route exact path='/recent' component={RecentList}/> 
 						</div>
 					</div>
 				</Switch>
